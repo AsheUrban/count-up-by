43a26b120cd56abee960080f8791cbade428153e
Ashe Urban <ashe@goldentongue.com>
2022-06-08 10:27:45 -0700

    ADD condition for negative numbers and pass test!
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,12 @@ function countUpBy (countBy, countTo) {
   if (noInput(countBy, countTo)) {
     return 0;
   }
+  if (parseInt(countBy) === NaN || parseInt(countTo) === NaN) {
+    return "NaN";
+  }
+  if (parseInt(countBy) <= 1 || parseInt(countTo) <= 0) {
+    return "Please don't";
+  }
   let multiples = [];
   for (let i = countBy; i <= countTo; i += countBy) {
     multiples.push(i);
